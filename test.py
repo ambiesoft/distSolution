0b121225b8ba64155c70adb8f5d17e7d678942f8
ambiesoft <ambiesoft.trueff@gmail.com>
2019-02-24 18:10:40 +0900

    test

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
 from funcs import myexit,showDiffAndExit,getAsFullpath,IsRemoteArchiveExists,getChangeLog
 
+# TEST 
+
 IsRemoteArchiveExists('https://ambiesoft.fam.cx/ffdav/uploads/dicregate/Dicregate-3.6.6.exe');
