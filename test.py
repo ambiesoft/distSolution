d7fa4dc1b0d8ce09eed0dee28b2028bf80a0867d
ambiesoft <ambiesoft.trueff@gmail.com>
2019-06-24 20:11:52 +0900

    fix updatebbs

--- test.py
+++ test.py
@@ -1,6 +1,12 @@
 import daver
-from funcs import myexit,showDiffAndExit,getAsFullpath,IsRemoteArchiveExists,getChangeLog
+from funcs import myexit, showDiffAndExit, getAsFullpath, IsRemoteArchiveExists, getChangeLog
+from updateBBS import updateBBS
 # TEST 
 
-daver.dupload('https://ambiesoft.fam.cx/ffdav/uploads/dicregate/Dicregate-3.6.6.exe', 'C:\\Linkout\\ddd.exe')
-IsRemoteArchiveExists('https://ambiesoft.fam.cx/ffdav/uploads/dicregate/Dicregate-3.6.6.exe');
+# daver.dupload('https://ambiesoft.fam.cx/ffdav/uploads/dicregate/Dicregate-3.6.6.exe', 'C:\\Linkout\\ddd.exe')
+# IsRemoteArchiveExists('https://ambiesoft.fam.cx/ffdav/uploads/dicregate/Dicregate-3.6.6.exe');
+
+print(updateBBS('aaa',
+        'v1.1.1',
+        'https://ambiesoft.fam.cx/ffdav/uploads/dicregate/Dicregate-3.6.6.exe',
+        'hist'))
