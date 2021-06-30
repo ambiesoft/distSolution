48c9e861ef8791187eb0e765604afe97394f645f
ambiesoft <ambiesoft.trueff@gmail.com>
2021-07-01 02:28:45 +0900

    bbs url

--- updateBBS.py
+++ updateBBS.py
@@ -4,7 +4,7 @@ import time
 # from _sqlite3 import version
 import random
 
-BBSULR='http://ambiesoft.mooo.com/minibbs/minibbs.php'
+BBSULR='https://ambiesoft.com/minibbs/minibbs.php'
 
 # https://gist.github.com/masahitojp/863593
 def wbs_request(method_string, url, args={}):
