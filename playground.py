cc80a34536f4cffece06b4ad91e6e0335c27b64e
ambiesoft <ambiesoft.trueff@gmail.com>
2021-07-19 13:04:40 +0900

    nazo

--- playground.py
+++ playground.py
@@ -1,4 +1,5 @@
 import sys
+import argparse
 
 def testFormat():
   code = '''
@@ -13,6 +14,20 @@ def testFormat():
 
 
 def main():
+
+  parser = argparse.ArgumentParser(prog='MyPythonTest', description='Test ArgumentParser')
+  parser.add_argument('-f', 
+                      action='store_true',
+                      help='do it with force')
+
+  args = parser.parse_args()
+
+  if args.f:
+    print('f is set')
+  else:
+    print('f is not set')
+
+def aaa():
   if not sys.stdin.isatty():
     print("not sys.stdin.isatty")
   else:
