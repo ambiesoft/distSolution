b9494526764c67ace440dc6533f83b0a26eca460
ambiesoft <ambiesoft.trueff@gmail.com>
2022-05-15 08:47:43 +0900

    Open 'history' with 'utf-8-sig'
    'utf-8-sig' opens files which has utf8-bommed files.
    It also properly opens and reads non-utf8-bommed files.

--- funcs.py
+++ funcs.py
@@ -4,8 +4,10 @@ import urllib
 import urllib.request
 import certifi
 
+
 def getAsFullpath(files, outdir):
     return [os.path.realpath(os.path.join(outdir, item)) for item in files]
+
 
 def getFileCount(d):
     total = 0
@@ -14,33 +16,36 @@ def getFileCount(d):
     return total
 
 # https://stackoverflow.com/a/6486513
+
+
 def getPathDiffs(first, second):
     first = set(first)
     second = set(second)
-    return ([item for item in  second if item not in first],[item for item in first if item not in second])
+    return ([item for item in second if item not in first], [item for item in first if item not in second])
+
 
 def getFileListAsFullPath(outdir):
     f = []
     for (dirpath, _, filenames) in os.walk(outdir):
-      for name in filenames:
-        f.append(os.path.realpath( os.path.join(dirpath, name)))
+        for name in filenames:
+            f.append(os.path.realpath(os.path.join(dirpath, name)))
 
     # return [f for f in os.listdir(outdir) if os.path.isfile(os.path.join(outdir, f))]
     return f
 
 
 def myexit(message, retval=1):
-    
+
     print('DistError: ' + message)
     # input('Press ENTER to exit')
     exit(retval)
-    
-    
+
+
 def showDiffAndExit(outdir, shouldBeFull, shouldTotal, exact):
-    
+
     current = getFileListAsFullPath(outdir)
-    (targetOver, listOver) = getPathDiffs(shouldBeFull,current)
-    
+    (targetOver, listOver) = getPathDiffs(shouldBeFull, current)
+
     message = ''
     if(targetOver):
         message += "Target directory contains following unlisted files:\n"
@@ -49,51 +54,54 @@ def showDiffAndExit(outdir, shouldBeFull, shouldTotal, exact):
         message == "\n"
         message += "[ShouldBeFiles] list contains following non-existent files:\n"
         message += "\n".join(str(e) for e in listOver)
-    
+
     if(exact):
         if(message):
             myexit(message)
     else:
-        message = "TotalFileCount different. ({} != {})\n".format(shouldTotal, getFileCount(outdir)) + message
-        myexit(message)    
-        
-        
+        message = "TotalFileCount different. ({} != {})\n".format(
+            shouldTotal, getFileCount(outdir)) + message
+        myexit(message)
+
+
 def IsRemoteArchiveExists(url):
-    
+
     request = urllib.request.Request(url)
-    request.get_method = lambda : 'HEAD'
-    
+    request.get_method = lambda: 'HEAD'
+
     status = 0
     try:
         responce = urllib.request.urlopen(request, cafile=certifi.where())
         status = responce.status
     except urllib.error.HTTPError as e:
         status = e.code
-        
-    if status==200:
+
+    if status == 200:
         return True
-    elif status==404:
-        # OK 
+    elif status == 404:
+        # OK
         return False
     else:
-        myexit("HEAD request returns invalid status {0}.".format(responce.status))
+        myexit("HEAD request returns invalid status {0}.".format(
+            responce.status))
+
 
 def doesStartWithDate(line):
     ''' returns true if line start with date'''
     return re.match('^\d\d\d\d/\d\d/\d\d', line)
 
+
 def getChangeLog(fileName, regstr):
     rets = []
     getFirst = False
-    with open(fileName, "r", encoding="utf-8") as f:
+    with open(fileName, "r", encoding="utf-8-sig") as f:
         for line in f.readlines():
             m = re.search(regstr, line)
-        
+
             if m and m.group(0) and doesStartWithDate(line):
                 if not getFirst:
-                    getFirst=True
+                    getFirst = True
                     continue
                 return ''.join(rets)
             rets.append(line)
-    return ''.join(rets)            
-
+    return ''.join(rets)
